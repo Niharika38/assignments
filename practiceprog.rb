0d0dfdd988d22a8b48a6709c8bec7fa02d4aa6a3
Niharika38 <you@example.com>
2022-02-15 21:41:13 +0530

    comment

--- practiceprog.rb
+++ practiceprog.rb
@@ -2,7 +2,7 @@
 area = 0
 print "Input the radius of the circle: "
 radius = gets.to_i
-perimeter = 2 * 3.14 * radius
-area = 3.14 * radius * radius
+perimeter = 3 * 3.14 * radius
+area = 3.1421 * radius * radius
 puts "The perimeter is #{perimeter}."
 puts "The area is #{area}."
