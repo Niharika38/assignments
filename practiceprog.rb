026b40ee7067257cd3a403648385422faa93a3df
Niharika38 <you@example.com>
2022-02-15 21:27:08 +0530

    first commit

---
+++ practiceprog.rb
@@ -0,0 +1,8 @@
+
+area = 0
+print "Input the radius of the circle: "
+radius = gets.to_i
+perimeter = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+puts "The perimeter is #{perimeter}."
+puts "The area is #{area}."
