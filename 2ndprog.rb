90cfcb8dcb299403be29fb43e0a6387b82c20ea9
Niharika38 <you@example.com>
2022-02-16 21:34:44 +0530

    comment

--- 2ndprog.rb
+++ 2ndprog.rb
@@ -37,3 +37,29 @@ def geeks
 
 end
 geeks
+
+# Ruby program to illustrate blocks
+
+# Method with single yield statement
+def print_once
+	
+	# statement of method
+	puts"Hello! I am Method"
+	
+# using yield statement
+yield
+end
+
+# Block 1
+print_once { puts "Hello! I am Block 1" }
+
+# Method with two yield statement
+def print_twice
+	
+# using yield statement
+yield
+yield
+end
+
+# Block 2
+print_twice { puts "Hello! I am Block 2" }
